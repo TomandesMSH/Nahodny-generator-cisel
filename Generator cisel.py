690822d674f3fc24d7c0e24ffad9c559d0a1101d
Tomandes Vanek <89749814+TomandesMSH@users.noreply.github.com>
2023-06-04 10:55:09 +0200

    Add files via upload

---
+++ Generator cisel.py
@@ -0,0 +1,9 @@
+import sys
+print("vitej v epickem number generatoru. umim cisla od jedny do 100. to muzes upravit v pythonu kdyz zmenis v 5 radku cislo")
+input("Klikni na enter pro vygenerovani cisla")
+import random
+
+print(random.randint(0,100))
+print("To je tvoje cislo")
+input("Stiskni enter pro ukonceni programu")
+sys.exit() 
